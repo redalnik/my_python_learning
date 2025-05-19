c49ba321910a0bcdf0d8839621b8e88f2a3dfbd7
Александр <redalnik@gmail.com>
2025-05-19 21:41:40 +0300

    Create second_lesson.py

---
+++ second_lesson.py
@@ -0,0 +1,12 @@
+# Программа расчета количества ингредиентов на заданное число гостей.
+sugar = 3 # ст.лож.
+eggs = 1 # штук
+flour = 3 # ст.лож.
+apples = 1 # штук
+
+guests = 4
+
+print("Сахар", sugar*guests, "ст.лож." )
+print("Яйца", eggs*guests, "шт." )
+print("Мука", flour*guests, "ст.лож.")
+print("Яблоки", apples*guests, "шт.")
